fc93ca28b7eb78037175ec94d38f112b61157f5a
Homa N <homa@nowhere.com>
2020-04-27 22:35:59 -0400

    adding all files

--- Mission_to_Mars/.ipynb_checkpoints/scrape_mars-checkpoint.py
+++ Mission_to_Mars/.ipynb_checkpoints/scrape_mars-checkpoint.py
@@ -1,32 +1,134 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.7.4"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 4
-}
+from bs4 import BeautifulSoup as bs
+import requests
+from splinter import Browser
+import pandas as pd
+
+# global dictionary to store all results
+mars_scrape_dict = {}
+
+def init_browser():
+    executable_path = {"executable_path": "chromedriver"}
+    return Browser("chrome", **executable_path, headless=True)
+
+# Scrape the NASA Mars News Site and collect the latest News Title and Paragraph
+def scrape_Mars_news():
+
+    url = 'https://mars.nasa.gov/news'
+    browser = init_browser()
+    browser.visit(url)
+    html = browser.html
+    soup = bs(html, 'html.parser')
+    
+    try:
+        title = soup.find_all('div', class_='content_title')[1].text
+        paragraph= soup.find_all('div', class_='article_teaser_body')[0].text
+        
+        mars_scrape_dict['news_title'] = title
+        mars_scrape_dict['news_body'] = paragraph
+
+    except AttributeError as e:
+        return(e)
+             
+    finally:
+        browser.quit()
+        
+# navigate the Jet Propulsion Lab site and find the image url for the current Featured Mars Image
+def scrape_featured_image():
+             
+    url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    browser = init_browser()
+    browser.visit(url)
+    html = browser.html
+    soup = bs(html, 'html.parser')
+
+    try:
+        url = soup.find_all('div', class_='carousel_items')[0].a.get('data-fancybox-href')
+        featured_image_url = "https://www.jpl.nasa.gov" + url
+        
+        mars_scrape_dict['featured_image'] = featured_image_url
+    
+    except AttributeError as e:
+        print(e)
+    
+    finally:
+        browser.quit()
+
+# Visit the Mars Weather twitter account here and scrape the latest Mars weather tweet from the page      
+def scrape_Mars_Weather():
+             
+    url= 'https://twitter.com/marswxreport?lang=en'
+    response = requests.get(url)
+    soup = bs(response.text, 'html.parser')
+
+    try:
+        mars_weather = soup.find_all('div', class_='js-tweet-text-container')[0].p.text       
+        mars_scrape_dict['mars_weather'] = mars_weather
+    
+    except AttributeError as e:
+        print(e)
+
+# Visit the Mars Facts webpage here and use Pandas to scrape the table containing facts about the plane
+def scrape_Mars_Facts():
+    
+    url = 'https://space-facts.com/mars/'
+    
+    try:
+        facts = pd.read_html(url)[0]
+        facts = facts.rename(columns={0:'Property', 1: 'value'})
+        facts = facts.set_index('Property')
+        facts_html = facts.to_html()
+        mars_scrape_dict['mars_facts'] = facts_html
+        
+    except AttributeError as e:
+        print(e)
+        
+# Visit the USGS Astrogeology site here to obtain high resolution images for each of Mar's hemispheres        
+def scrape_Mars_hemispheres():
+   
+
+    base_url = 'https://astrogeology.usgs.gov'
+    url= 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    
+    browser = init_browser()
+    
+    response = requests.get(url)
+    soup = bs(response.text, 'html.parser')
+
+    full_image_list=[]
+
+    i=1
+
+    try:
+        Hemispheres = soup.find_all('a', class_='itemLink product-item')
+    
+        for i in range(4):
+
+            # Store title of image
+            title = Hemispheres[i].text.strip('Enhanced ')
+            # get the image url on the main page and append to the base url for a complete url reference
+            image_url = base_url + Hemispheres[i]['href']
+
+            # Open the full resolution image page
+            browser.visit(image_url)
+            html = browser.html
+            soup = bs( html, 'html.parser')
+
+            # find the full resolution image
+            full_resolution_url = soup.find('img', class_='wide-image')['src']
+
+            # Append the information in a list of dictionaries 
+            full_image_list.append({'title' : title, 'img_url' : base_url + full_resolution_url})  
+         
+        mars_scrape_dict['hemispheres'] = full_image_list         
+    except AttributeError as e:
+        print(e)
+
+def scrape():                                     
+    
+    scrape_Mars_news()
+    scrape_featured_image()
+    scrape_Mars_Weather()
+    scrape_Mars_Facts()
+    scrape_Mars_hemispheres()
+    return mars_scrape_dict
+
--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -1,71 +1,30 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "from flask import Flask, render_template, redirect\n",
-    "from flask_pymongo import PyMongo\n",
-    "import scrape_mars\n",
-    "\n",
-    "# Create an instance of Flask\n",
-    "app = Flask(__name__)\n",
-    "\n",
-    "# Use flask_pymongo to set up mongo connection\n",
-    "app.config[\"MONGO_URI\"] = \"mongodb://localhost:27017/Mars_app\"\n",
-    "mongo = PyMongo(app)\n",
-    "\n",
-    "# Route to render index.html template using data from Mongo\n",
-    "@app.route(\"/\")\n",
-    "def home():\n",
-    "\n",
-    "    # Find one record of data from the mongo database\n",
-    "    Mars_record = mongo.db.collection.find_one()\n",
-    "\n",
-    "    # Return template and data\n",
-    "    return render_template(\"index.html\", Mars_data=Mars_record)\n",
-    "\n",
-    "# Route that will trigger the scrape function\n",
-    "@app.route(\"/scrape\")\n",
-    "def scrape():\n",
-    "\n",
-    "    m = mongo.db.Mars_record    \n",
-    "    # Run the scrape function\n",
-    "    mars_data = scrape_mars.scrape()\n",
-    "    \n",
-    "    # Update the Mongo database using update and upsert=True\n",
-    "    m.update({}, mars_data, upsert=True)\n",
-    "\n",
-    "    # Redirect back to home page\n",
-    "     return redirect(\"/\", code=302)\n",
-    "\n",
-    "\n",
-    "if __name__ == \"__main__\":\n",
-    "    app.run(debug=True)\n"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.7.4"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 4
-}
+from flask import Flask, render_template, jsonify, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
+
+app = Flask(__name__)
+
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+
+# Route to render index.html template using data from Mongo
+@app.route("/")
+def home():
+    # Find one record of data from the mongo database
+    Mars_record = mongo.db.Mars_record.find_one()
+    # Return template and data
+    return render_template("index.html", mission_data=Mars_record)
+
+# Route that will trigger the scrape function
+@app.route("/scrape")
+def scrape():
+    m = mongo.db.Mars_record
+    # Run the scrape function
+    mars_data = scrape_mars.scrape()
+    # Update the Mongo database using update and upsert=True
+    m.replace_one({}, mars_data, upsert=True)
+
+    # Redirect back to home page
+    return redirect("/", code=302)
+
+if __name__ == "__main__":
+    app.run(debug=True)
